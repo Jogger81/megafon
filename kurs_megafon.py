d58e65ecb30f8f804bb5b48cf602c4e82a101b55
y-zZz-y <avk@avkst.ru>
2022-02-10 10:28:12 +0500

    upd

--- kurs_megafon.py
+++ kurs_megafon.py
@@ -18,7 +18,7 @@ def createParser():
     parser.add_argument('-m', '--model', dest="model", default='model.sav')
     parser.add_argument('-t', '--test', dest="data_test", default='data_test.csv')
     parser.add_argument('-f', '--features', dest="data_features", default='features.csv')
-    parser.add_argument('-d', '--destination', dest="destination", default='answers_test.csv')
+    parser.add_argument('-dest', '--destination', dest="destination", default='answers_test.csv')
     parser.add_argument('-d', '--directory', dest="filepath", default='./')
     return parser
 
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     print('features file = '+namespace.data_features)
     print('file path = '+namespace.filepath)
     print('destination = '+namespace.destination)
-    print('-' * 29)
+    print('-' * 30)
 
     print('Обработка Data_test 1/7')
     data_test = dd.read_csv(namespace.filepath+namespace.data_test).drop(columns=['Unnamed: 0'])
@@ -61,15 +61,26 @@ if __name__ == '__main__':
     print('Data_test join with features 3/7')
     X_test = data_test.join(df, on="id", rsuffix='_other').compute()
 
-    print('Загружаем модель 4/7')
-    loaded_model = pickle.load(open(namespace.filepath+namespace.model, 'rb'))
+    X_nunique = X_test.apply(lambda x: x.nunique(dropna=False))
+    f_all = set(X_nunique.index.tolist())
 
-    print('Получаем предсказание 5/7')
-    model_pred = loaded_model.predict_proba(X_test)
+    if isinstance(X_test, pd.DataFrame):
+        print('Загружаем модель 4/7')
+        loaded_model = pickle.load(open(namespace.filepath + namespace.model, 'rb'))
 
-    print('Получаем предсказание 6/7')
-    X_test = X_test[['buy_time', 'id', 'vas_id']]
-    X_test['target'] = model_pred[:, 1]
+        print('Получаем предсказание 5/7')
+        model_pred = loaded_model.predict_proba(X_test)
 
-    print('Получаем предсказание 7/7')
-    X_test.to_csv(namespace.filepath+namespace.destination)
+        print('Подготавливаем данные 6/7')
+        X_test = X_test[['buy_time', 'id', 'vas_id']]
+        X_test['target'] = model_pred[:,1]
+
+        print('Сохраняем в файл 7/7')
+        X_test.to_csv(namespace.filepath + namespace.destination)
+
+        print('ГОТОВО!')
+    else:
+        print('ОШИБКА ДАТАСЕТА')
+
+
+
